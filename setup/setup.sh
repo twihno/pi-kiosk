894159175d9183d0a59f8c5cc2a70362a8eff6f6
Thomas Schuster <twihno@gmail.com>
2021-10-25 22:48:47 +0200

    feat: added raspi-config steps

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 VAR_COUNTRY="DE"
-VAR_TIMEZONE="Europe/Berlin"
+VAR_TIMEZONE="Europe/Berlin" # timedatectl list-timezones
 VAR_HOSTNAME="raspikiosk"
+VAR_KEYMAP="de"
+VAR_LOCALE="de_DE.UTF-8 UTF-8" # /usr/share/i18n/SUPPORTED
 
 echo -e "---Raspberry Pi Firefox Kiosk Setup---\n"
 
@@ -14,7 +16,7 @@ then
 fi
 
 # Upgrade every package
-apt-get update && apt-get dist-upgrade -y && apt-get autoremove
+apt-get update && apt-get dist-upgrade -y && apt-get autoremove -y && apt-get clean -y
 
 # Set timezone
 timedatectl set-timezone $VAR_TIMEZONE
@@ -22,3 +24,8 @@ timedatectl set-timezone $VAR_TIMEZONE
 # Config raspi
 raspi-config nonint do_wifi_country $VAR_COUNTRY
 raspi-config nonint do_hostname $VAR_HOSTNAME
+raspi-config nonint do_overscan 0
+raspi-config nonint do_configure_keyboard $VAR_KEYMAP
+raspi-config nonint do_change_locale $VAR_LOCALE
+
+
